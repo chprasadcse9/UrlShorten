0a5aebe413714ca82db821d1ea2989ac7b7faf4e
prasadchk9 <prasad.ch@kloud9.nyc>
2023-01-16 21:43:36 +0530

    commit

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get('mongodb://localhost:27017/urls');
+const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
